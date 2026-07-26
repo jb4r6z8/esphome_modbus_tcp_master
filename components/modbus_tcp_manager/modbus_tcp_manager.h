79d6952df4c7f5dc903d75f50dbae080f0162c5f
jb4r6z8 <97252913+jb4r6z8@users.noreply.github.com>
2026-07-26 12:06:27 +0200

    Update modbus_tcp_manager.h

--- components/modbus_tcp_manager/modbus_tcp_manager.h
+++ components/modbus_tcp_manager/modbus_tcp_manager.h
@@ -136,7 +136,7 @@ public:
         if (!send_data(sock, request)) {
             ::close(sock);
             response.error_message = "Send failed";
-            is_connected_ = false;
+            is_connected_ = true;
             return response;
         }
 
