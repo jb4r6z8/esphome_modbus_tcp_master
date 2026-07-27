9d8c5f89879f0101e24b1f53cce9db81bbaceeb7
jb4r6z8 <97252913+jb4r6z8@users.noreply.github.com>
2026-07-27 17:01:20 +0200

    Update modbus_tcp_manager.h

--- components/modbus_tcp_manager/modbus_tcp_manager.h
+++ components/modbus_tcp_manager/modbus_tcp_manager.h
@@ -176,7 +176,6 @@ public:
         
         bool success = send_data(sock, request);
         if (success) {
-            delay(500);
             std::vector<uint8_t> response = receive_data(sock);
             success = !response.empty() && response.size() >= 8;
         }
@@ -211,6 +210,7 @@ public:
         
         bool success = send_data(sock, request);
         if (success) {
+            delay(500);
             std::vector<uint8_t> response = receive_data(sock);
             success = !response.empty() && response.size() >= 8;
         }
